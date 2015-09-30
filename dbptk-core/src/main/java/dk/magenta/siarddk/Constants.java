3d8ee71c40f396f822997c3e8a513c0655fe1ed4
Andreas Kring <andreas@magenta.dk>
2015-09-30 20:09:40 +0200

    Changed constants

--- dbptk-core/src/main/java/dk/magenta/siarddk/Constants.java
+++ dbptk-core/src/main/java/dk/magenta/siarddk/Constants.java
@@ -6,7 +6,7 @@ package dk.magenta.siarddk;
  */
 public class Constants {
 
-  // Name of the context documentation folder in the archive
+  // Name  of the context documentation folder in the archive
   public static final String CONTEXT_DOCUMENTATION_RELATIVE_PATH = "ContextDocumentation";
 
   // Key for context documentation folder (given on command line)
